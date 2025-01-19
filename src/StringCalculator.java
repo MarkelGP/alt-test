25dd3aa33a41576e28c62278ea649a65f6341e86
MarkelGP <markelov.gena81@gmail.com>
2025-01-19 16:04:16 +0300

    test otpravki3

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//ddddddd
 public class StringCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
